32cde6062bb4c1749742ecd35ea12857658cd71a
vuminhtrinh2804 <vuminhtrinh2804@gmail.com>
2023-01-09 17:16:41 +0700

    feat: add snippets for luasnip, uncomment neovide settings caller

--- init.lua
+++ init.lua
@@ -4,9 +4,7 @@ require('config.statusline-winbar')
 require('config.diagnostics')
 require('config.lazy')
 
---[[
 if vim.g.neovide ~= nil then
     print('Runing inside Neovide')
     require('config.settings-neovide')
 end
---]]
