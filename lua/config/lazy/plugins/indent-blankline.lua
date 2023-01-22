d7365b1cd67b1602beaeeef476828e52b9e5f6b8
vuminhtrinh2804 <vuminhtrinh2804@gmail.com>
2023-01-22 08:11:05 +0700

    change: better indent guide char, add option to show leading spaces, fix format

--- lua/config/lazy/plugins/indent-blankline.lua
+++ lua/config/lazy/plugins/indent-blankline.lua
@@ -1,6 +1,8 @@
 local M = { 'lukas-reineke/indent-blankline.nvim',
     event = 'BufReadPost',
     config = function()
+        -- vim.opt.list = true
+        -- vim.opt.listchars = "lead:"
         require('indent_blankline').setup {
             buftype_exclude = { "terminal", "nofile" },
             filetype_exclude = {
@@ -10,30 +12,30 @@ local M = { 'lukas-reineke/indent-blankline.nvim',
 
             -- , '┆', '┊' ┆ ┇ ┊ ┋ ┆ ┇ ┊ ┋ '|', '¦', '┆', '┊' ⋅
             char = "┊",
-            show_trailing_blankline_indent = true,
+            show_trailing_blankline_indent = false,
             use_treesitter = true,
             space_char_blankline = ' ',
-             context_patterns = {
-              "class",
-              "return",
-              "function",
-              "method",
-              "^if",
-              "^while",
-              "jsx_element",
-              "^for",
-              "^object",
-              "^table",
-              "block",
-              "arguments",
-              "if_statement",
-              "else_clause",
-              "jsx_element",
-              "jsx_self_closing_element",
-              "try_statement",
-              "catch_clause",
-              "import_statement",
-              "operation_type",
+            context_patterns = {
+                "class",
+                "return",
+                "function",
+                "method",
+                "^if",
+                "^while",
+                "jsx_element",
+                "^for",
+                "^object",
+                "^table",
+                "block",
+                "arguments",
+                "if_statement",
+                "else_clause",
+                "jsx_element",
+                "jsx_self_closing_element",
+                "try_statement",
+                "catch_clause",
+                "import_statement",
+                "operation_type",
             },
         }
     end
