fa28c38f7a6e0bb4accc2c5998cbe0efdba872b9
vuminhtrinh2804 <vuminhtrinh2804@gmail.com>
2023-02-19 13:54:02 +0700

    change: better font for neovide

--- lua/config/settings-neovide.lua
+++ lua/config/settings-neovide.lua
@@ -1,7 +1,4 @@
-vim.o.guifont = 'Iosevka Fixed SS15 Semibold:h12'
---vim.g.neovide_floating_blur_amount_x = 2.0
---vim.g.neovide_floating_blur_amount_y = 2.0
+vim.o.guifont = 'Iosevka SS15 Semibold:h12'
 vim.g.neovide_transparency = 1
 vim.g.neovide_hide_mouse_when_typing = true
-vim.g.neovide_underline_automatic_scaling = true
 vim.g.neovide_scale_factor = 1.0
