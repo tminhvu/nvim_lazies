4023932ff9d25daa05c241a5d7b5821d6ec77a6f
vuminhtrinh2804 <vuminhtrinh2804@gmail.com>
2023-02-26 15:00:16 +0700

    fix: typo in symbol outline

--- lua/config/lazy/plugins/tokyonight.lua
+++ lua/config/lazy/plugins/tokyonight.lua
@@ -20,7 +20,7 @@ local M = {
                 sidebars = "dark", -- style for sidebars, see below
                 floats = "dark", -- style for floating windows
             },
-            sidebars = { "nvimtree", "help", "terminal", "outline" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+            sidebars = { "nvimtree", "help", "terminal", "Outline" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
             day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
             hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
             dim_inactive = false, -- dims inactive windows
