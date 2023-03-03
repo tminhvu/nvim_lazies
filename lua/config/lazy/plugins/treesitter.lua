da04511c18c9d36dd16890ab981c80f3b0cbc65c
vuminhtrinh2804 <vuminhtrinh2804@gmail.com>
2023-03-03 15:37:14 +0700

    add: parens hightlights

--- lua/config/lazy/plugins/treesitter.lua
+++ lua/config/lazy/plugins/treesitter.lua
@@ -2,15 +2,16 @@ local M = {
     'nvim-treesitter/nvim-treesitter',
     event = 'BufReadPost',
     dependencies = {
-        'windwp/nvim-ts-autotag'
+        'windwp/nvim-ts-autotag',
+        'HiPhish/nvim-ts-rainbow2',
     },
-    config = function ()
-        require('nvim-treesitter.configs').setup{
+    config = function()
+        require('nvim-treesitter.configs').setup {
             ensure_installed = { "c", "java", "bash", "css", "lua", "html", "javascript", "typescript", "tsx", "json", "markdown", "markdown_inline" },
             sync_install = false,
             highlight = {
                 enable = true, -- false will disable the whole extension
-                additional_vim_regex_highlighting = true
+                additional_vim_regex_highlighting = false
             },
             incremental_selection = {
                 enable = false,
@@ -28,6 +29,15 @@ local M = {
                 move = {
                     enable = false,
                 },
+            },
+            rainbow = {
+                enable = true,
+                -- list of languages you want to disable the plugin for
+                --disable = {},
+                -- Which query to use for finding delimiters
+                query = 'rainbow-parens',
+                -- Highlight the entire buffer all at once
+                strategy = require 'ts-rainbow.strategy.global',
             }
         }
     end
