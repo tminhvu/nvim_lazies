dccb72e27014a909eb4f07e1076dc8d8e47c3753
vuminhtrinh2804 <vuminhtrinh2804@gmail.com>
2023-03-06 21:10:09 +0700

    revert: no need for rainbows brackets

--- lua/config/lazy/plugins/treesitter.lua
+++ lua/config/lazy/plugins/treesitter.lua
@@ -3,7 +3,6 @@ local M = {
     event = 'BufReadPost',
     dependencies = {
         'windwp/nvim-ts-autotag',
-        'HiPhish/nvim-ts-rainbow2',
     },
     config = function()
         require('nvim-treesitter.configs').setup {
@@ -30,15 +29,6 @@ local M = {
                     enable = false,
                 },
             },
-            rainbow = {
-                enable = true,
-                -- list of languages you want to disable the plugin for
-                --disable = {},
-                -- Which query to use for finding delimiters
-                query = 'rainbow-parens',
-                -- Highlight the entire buffer all at once
-                strategy = require 'ts-rainbow.strategy.global',
-            }
         }
     end
 }
