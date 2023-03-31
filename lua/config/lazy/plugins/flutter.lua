9011fcb6a46fb6ae37513fa78be42f8d7f7e9deb
vuminhtrinh2804 <vuminhtrinh2804@gmail.com>
2023-03-31 15:37:52 +0700

    change: color highlights for flutter

--- lua/config/lazy/plugins/flutter.lua
+++ lua/config/lazy/plugins/flutter.lua
@@ -31,6 +31,14 @@ local M = {
                         '<cmd>lua vim.lsp.buf.format { async = true }<CR>',
                         opts)
                 end,
+                color = { -- show the derived colours for dart variables
+                    enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+                    background = true, -- highlight the background
+                    background_color = { r = 19, g = 17, b = 24 }, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
+                    foreground = false, -- highlight the foreground
+                    virtual_text = false, -- show the highlight using virtual text
+                    virtual_text_str = "", -- the virtual text character to highlight
+                },
                 capabilities = capabilities,
                 settings = {
                     showTodos = true,
