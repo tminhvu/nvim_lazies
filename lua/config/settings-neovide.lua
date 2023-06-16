99a1a51fe8f58971b3b190cfb65f107363ec80bb
vuminhtrinh2804 <vuminhtrinh2804@gmail.com>
2023-06-16 17:07:11 +0700

    change(neovide): smaller scale, tweeks, font change

--- lua/config/settings-neovide.lua
+++ lua/config/settings-neovide.lua
@@ -1,5 +1,10 @@
-vim.o.guifont = 'IosevkaTerm Nerd Font Semibold:h12'
+vim.o.guifont = 'IosevkaTerm Nerd Font:h12,IosevkaTerm Nerd Font Semibold:h12'
 --vim.g.neovide_transparency = 1
 vim.g.neovide_hide_mouse_when_typing = true
---vim.g.neovide_scale_factor = 1.0
+--vim.g.neovide_scale_factor = 0.75
 vim.g.neovide_cursor_antialiasing = false
+vim.g.neovide_remember_window_size = true
+vim.g.neovide_padding_top = 4
+vim.g.neovide_padding_bottom = 4
+vim.g.neovide_padding_right = 4
+vim.g.neovide_padding_left = 4
