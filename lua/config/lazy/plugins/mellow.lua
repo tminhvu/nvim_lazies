147244087bca926fc431ca32d3c0ae1182a4925b
vuminhtrinh2804 <vuminhtrinh2804@gmail.com>
2023-07-15 11:23:05 +0700

    fix: typo

--- lua/config/lazy/plugins/mellow.lua
+++ lua/config/lazy/plugins/mellow.lua
@@ -3,17 +3,17 @@ local M = {
     dir = '~/Dev/lua/mellow.nvim',
     lazy = false,
     config = function()
-        vim.gmellow_italic_comments = true   --Make comments italic
-        vim.gmellow_italic_keywords = false  --Make keywords italic
-        vim.gmellow_italic_booleans = false  --Make booleans italic
-        vim.gmellow_italic_functions = false --Make functions italic
-        vim.gmellow_italic_variables = false --Make variables italic
-        vim.gmellow_bold_comments = false    --	Make comments bold
-        vim.gmellow_bold_keywords = true     --	Make keywords bold
-        vim.gmellow_bold_booleans = true    --	Make booleans bold
-        vim.gmellow_bold_functions = true    --	Make functions bold
-        vim.gmellow_bold_variables = true   --	Make variables bold
-        vim.gmellow_transparent = false      --Disable background color
+        vim.g.mellow_italic_comments = true   --Make comments italic
+        vim.g.mellow_italic_keywords = false  --Make keywords italic
+        vim.g.mellow_italic_booleans = false  --Make booleans italic
+        vim.g.mellow_italic_functions = false --Make functions italic
+        vim.g.mellow_italic_variables = false --Make variables italic
+        vim.g.mellow_bold_comments = false    --	Make comments bold
+        vim.g.mellow_bold_keywords = true     --	Make keywords bold
+        vim.g.mellow_bold_booleans = true     --	Make booleans bold
+        vim.g.mellow_bold_functions = true    --	Make functions bold
+        vim.g.mellow_bold_variables = true    --	Make variables bold
+        vim.g.mellow_transparent = false      --Disable background color
         vim.cmd.colorscheme 'mellow'
     end
 }
